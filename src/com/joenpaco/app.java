c1a8a4c5639fae0cd274d7f2f809f4c8346c85a3
Jose Parico <joenpaco@gmail.com>
2021-12-11 14:42:14 -0300

    Primer commit

---
+++ src/com/joenpaco/app.java
@@ -0,0 +1,29 @@
+package com.joenpaco;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class app {
+    public static void main(String[] args) {
+        List<ArrayList<String>> listOfLists = new ArrayList<ArrayList<String>>();
+
+        ArrayList<String> list1 = new ArrayList<String>();
+        list1.add("Delhi");
+        list1.add("Mumbai");
+
+        listOfLists.add(list1);
+
+        ArrayList<String> anotherList = new ArrayList<String>();
+        anotherList.add("Beijing");
+        anotherList.add("Shanghai");
+
+        listOfLists.add(anotherList);
+
+        listOfLists.forEach((i)->{
+            i.forEach((j)->{
+                System.out.println(j);
+            });
+        });
+
+    }
+}
